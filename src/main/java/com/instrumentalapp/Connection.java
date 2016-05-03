74bd9a1f666bc8007edb1a91c4d32c98658c9dce
Elijah Miller <elijah.miller@gmail.com>
2016-05-03 17:16:21 -0400

    Clarify log message which can occur mid-connection.

--- src/main/java/com/instrumentalapp/Connection.java
+++ src/main/java/com/instrumentalapp/Connection.java
@@ -268,7 +268,7 @@ public final class Connection implements Runnable {
 		// Do a reconnect, then try to send the message again.
 		// Since we've removed the message from the queue, don't discard it, and don't advance.
 		long delay = (long) Math.min(maxReconnectDelay, Math.pow(errors++, reconnectBackoff) * 1000);
-		LOG.severe("Failed to connect to " + agentOptions.getHost() + ":" + agentOptions.getPort() + ". Retry in " + delay + "ms");
+		LOG.severe("Connection failed to " + agentOptions.getHost() + ":" + agentOptions.getPort() + ". Retry in " + delay + "ms");
 		Thread.sleep(delay);
 	}
 
